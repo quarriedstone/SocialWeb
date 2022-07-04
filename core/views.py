8cd083b023f52a87edc485dcdb57e8a4e26fd07e
quarriedstone <quarriedstone@gmail.com>
2022-07-04 14:10:11 +0300

    feature: added added signin and logout

--- core/views.py
+++ core/views.py
@@ -3,7 +3,10 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User, auth
 from .models import Profile
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
+
+@login_required(login_url='signin')
 def index(request):
     return render(request, 'index.html')
 
@@ -37,3 +40,26 @@ def signup(request):
             return redirect('signup')
     else:
         return render(request, 'signup.html')
+
+
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Credentials Invalid')
+            return redirect('signin')
+    else:
+        return render(request, 'signin.html')
+
+
+@login_required(login_url='signin')
+def logout(request):
+    auth.logout(request)
+    return redirect('signin')
